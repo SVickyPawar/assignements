e8f8b06af00ca8300b02e83f4ab321accc2fd743
Vivek Pawar <vickypawar2609@gmail.com>
2023-10-19 11:02:36 +0530

    cards changes init

--- small-joy-app/frontend/src/components/Card.jsx
+++ small-joy-app/frontend/src/components/Card.jsx
@@ -9,33 +9,94 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
+  Stack,
+  IconButton,
+  Input,
+  Textarea,
 } from '@chakra-ui/react';
+import { EditIcon, ViewIcon, DeleteIcon } from '@chakra-ui/icons';
 
 const Card = ({ title, content }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [editedContent, setEditedContent] = useState(content);
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
+  const handleOpenEditModal = () => {
+    setIsEditModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
+  const handleCloseEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
+  const handleOpenDetailsModal = () => {
+    setIsDetailsModalOpen(true);
+  };
+
+  const handleCloseDetailsModal = () => {
+    setIsDetailsModalOpen(false);
   };
 
   return (
     <Box borderWidth="1px" borderRadius="lg" p="4" m="2">
-      <Text fontWeight="bold">{title}</Text>
-      <Text>{content}</Text>
-      <Button onClick={handleOpenModal} mt="2" colorScheme="blue">
-        View Details
-      </Button>
+      <Stack spacing={2}>
+        <Text fontWeight="bold">{editedTitle}</Text>
+        {isDetailsModalOpen ? <Text>{editedContent}</Text> : <Text>{content.slice(0, 100)}</Text>}
+        <Button onClick={handleOpenDetailsModal} colorScheme="blue">
+          View Details
+        </Button>
+        <Stack direction="row" spacing={2}>
+          <IconButton
+            aria-label="Edit"
+            icon={<EditIcon />}
+            colorScheme="teal"
+            onClick={handleOpenEditModal}
+          />
+          <IconButton
+            aria-label="Delete"
+            icon={<DeleteIcon />}
+            colorScheme="red"
+            onClick={() => {
+              // Handle delete action here
+            }}
+          />
+        </Stack>
+      </Stack>
 
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal} size="lg">
+      <Modal isOpen={isEditModalOpen} onClose={handleCloseEditModal} size="lg">
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>{title}</ModalHeader>
+          <ModalHeader>Edit Card</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>{content}</ModalBody>
+          <ModalBody>
+            <Input
+              placeholder="Title"
+              value={editedTitle}
+              onChange={(e) => setEditedTitle(e.target.value)}
+            />
+            <Textarea
+              placeholder="Content"
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+              mt={4}
+            />
+            <Button colorScheme="teal" mt={4} onClick={handleCloseEditModal}>
+              Save
+            </Button>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={isDetailsModalOpen} onClose={handleCloseDetailsModal} size="lg">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Details</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold">{editedTitle}</Text>
+            <Text>{editedContent}</Text>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </Box>
