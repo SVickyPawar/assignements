58a6f213ffa637928316d527b1d99accc711040c
Vivek Pawar <vickypawar2609@gmail.com>
2023-11-04 17:55:29 +0530

    auth added for login

--- fastor-restaurant-app/src/components/Homepage.jsx
+++ fastor-restaurant-app/src/components/Homepage.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Homepage.module.css";
-import { Card, Stack, Image, Heading, Text, Button, CardBody ,CardFooter,Grid,GridItem} from '@chakra-ui/react';
-
+import {
+	Card,
+	Stack,
+	Image,
+	Heading,
+	Text,
+	Button,
+	CardBody,
+	CardFooter,
+	Grid,
+	GridItem,
+} from "@chakra-ui/react";
 
 const Homepage = () => {
-    const [resdata, setResdata] = useState([]);
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTgxMCIsImVtYWlsIjoiOTgxODk3OTQ1MEBmYXN0b3IuaW4iLCJwaG9uZSI6Ijk4MTg5Nzk0NTAiLCJkaWFsX2NvZGUiOiIrOTEiLCJsYXN0X3Bhc3N3b3JkX3VwZGF0ZSI6IjIwMjAtMTItMTlUMTE6MTM6MjQuMDAwWiIsImlhdCI6MTY5OTA3NzI1NiwiZXhwIjoxNzA2MzM0ODU2fQ.bdGB6gYuLIkLiC_reFgp6S0RxjDSs67tf_NEb_fSo00"
-		useEffect(() => {
-			fetch("https://staging.fastor.in/v1/m/restaurant?city_id=118");
+	const [resdata, setResdata] = useState([]);
+	const token =
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMTgxMCIsImVtYWlsIjoiOTgxODk3OTQ1MEBmYXN0b3IuaW4iLCJwaG9uZSI6Ijk4MTg5Nzk0NTAiLCJkaWFsX2NvZGUiOiIrOTEiLCJsYXN0X3Bhc3N3b3JkX3VwZGF0ZSI6IjIwMjAtMTItMTlUMTE6MTM6MjQuMDAwWiIsImlhdCI6MTY5OTA3NzI1NiwiZXhwIjoxNzA2MzM0ODU2fQ.bdGB6gYuLIkLiC_reFgp6S0RxjDSs67tf_NEb_fSo00";
+	useEffect(() => {
+		fetch("https://staging.fastor.in/v1/m/restaurant?city_id=118");
 
-			fetch("https://staging.fastor.in/v1/m/restaurant?city_id=118", {
-				headers: { Authorization: `Bearer ${token}` },
-			})
-				.then((resp) => resp.json())
-				.then((json) => setResdata(json));
-		}, []);
+		fetch("https://staging.fastor.in/v1/m/restaurant?city_id=118", {
+			headers: { Authorization: `Bearer ${token}` },
+		})
+			.then((resp) => resp.json())
+			.then((json) => {setResdata(json);console.log(json)});
+	}, []);
 
 	return (
 		<div className={styles.main}>
@@ -54,43 +65,140 @@ const Homepage = () => {
 				</div>
 			</div>
 
-            <div className={styles.box}>
-                <div className={styles.boxText}>
-                    <p>Your tase</p>
-                    <p> <span><i className="fas fa-chevron-right"></i></span>see all</p>
-                </div>
-                <div className={styles.diplayGrid}>
-                <div className={styles.verticalCard}>
-                    <img src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""  />
-                    <div style={{width:'100%',height:'70px',lineHeight:'14px',backgroundColor: '#FFB6C1'}}>
-                        <p style={{padding:'10px 0px 0px 10px'}}>Nik Bakers</p>
-                        <p style={{fontSize:"12px",lineHeight:'14px',paddingLeft:'10px'}}>Cannought Place New Delhi</p>
-                    </div>
-                </div>
-                <div className={styles.verticalCard}>
-                    <img src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""  />
-                    <div style={{width:'100%',height:'70px',lineHeight:'14px',backgroundColor: '#FFB6C1'}}>
-                        <p style={{padding:'10px 0px 0px 10px',}}>Nik Bakers</p>
-                        <p style={{fontSize:"12px",lineHeight:'14px',paddingLeft:'10px',}}>Cannought Place New Delhi</p>
-                    </div>
-                </div>
-                <div className={styles.verticalCard}>
-                    <img src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""  />
-                    <div style={{width:'100%',height:'70px',lineHeight:'14px',backgroundColor: '#FFB6C1'}}>
-                        <p style={{padding:'10px 0px 0px 10px',}}>Nik Bakers</p>
-                        <p style={{fontSize:"12px",lineHeight:'14px',paddingLeft:'10px',}}>Cannought Place New Delhi</p>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div className={styles.tryNowpic}>
-            <img src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt=""  width="95%" height="100%" style={{objectFit:'cover',margin:'auto',borderRadius:'18px'}} />
-                
-            </div>
-            <div>
-                <h6>Popular Ones</h6>
-                
-            </div>
+			<div className={styles.box}>
+				<div className={styles.boxText}>
+					<p>Your tase</p>
+					<p>
+						{" "}
+						<span>
+							<i className="fas fa-chevron-right"></i>
+						</span>
+						see all
+					</p>
+				</div>
+				<div className={styles.diplayGrid}>
+					<div className={styles.verticalCard}>
+						<img
+							src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+							alt=""
+						/>
+						<div
+							style={{
+								width: "100%",
+								height: "70px",
+								lineHeight: "14px",
+								backgroundColor: "#FFB6C1",
+							}}
+						>
+							<p style={{ padding: "10px 0px 0px 10px" }}>Nik Bakers</p>
+							<p
+								style={{
+									fontSize: "12px",
+									lineHeight: "14px",
+									paddingLeft: "10px",
+								}}
+							>
+								Cannought Place New Delhi
+							</p>
+						</div>
+					</div>
+					<div className={styles.verticalCard}>
+						<img
+							src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+							alt=""
+						/>
+						<div
+							style={{
+								width: "100%",
+								height: "70px",
+								lineHeight: "14px",
+								backgroundColor: "#FFB6C1",
+							}}
+						>
+							<p style={{ padding: "10px 0px 0px 10px" }}>Nik Bakers</p>
+							<p
+								style={{
+									fontSize: "12px",
+									lineHeight: "14px",
+									paddingLeft: "10px",
+								}}
+							>
+								Cannought Place New Delhi
+							</p>
+						</div>
+					</div>
+					<div className={styles.verticalCard}>
+						<img
+							src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+							alt=""
+						/>
+						<div
+							style={{
+								width: "100%",
+								height: "70px",
+								lineHeight: "14px",
+								backgroundColor: "#FFB6C1",
+							}}
+						>
+							<p style={{ padding: "10px 0px 0px 10px" }}>Nik Bakers</p>
+							<p
+								style={{
+									fontSize: "12px",
+									lineHeight: "14px",
+									paddingLeft: "10px",
+								}}
+							>
+								Cannought Place New Delhi
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={styles.tryNowpic}>
+				<img
+					src="https://s3-alpha-sig.figma.com/img/a25f/9d25/01c2dd2d51c72422508105fa7b2d079f?Expires=1699833600&Signature=J~Y8xP5AidOc1kRcaG-Si4M7elAGqBMyA6FWVxIH5GjxNnsMBX1-AWVy1ai09xv2U9l58acqQEW5-QJ2GUFXs-sPkwewXyOqwDYcfcvZxbPdgyhs-PgvTAwJx7U4EO5DyioyJTRcBtWVgTqmoDdkW5cKvgEnKdFb-j9Arght9Dwb2SGUTZLgEvKrB7LPI2xXAidnZHmWnN3gTY0wloP071WMa449jaelloioc3bwFMlYZZwI03~pRkKtfwi8Kl9YWyUB6tXq4nXZHSb1H2B4ZkattsQAnlxl-ncUChldTo-aN1XpgX8H~HJbjd5-d~mwlnTcXbV0U4YBptcBWb1kkg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+					alt=""
+					width="95%"
+					height="100%"
+					style={{ objectFit: "cover", margin: "auto", borderRadius: "18px" }}
+				/>
+			</div>
+			<div>
+				<h6>Popular Ones</h6>
+				<Grid
+					templateColumns="repeat(auto-fit, minmax(250px, 1fr))"
+					gap={4}
+				>
+					{resdata.length &&
+						resdata.map((item) => (
+							<Card
+								key={item.id}
+								variant="outline"
+							>
+								<Image
+									objectFit="cover"
+									src={item.images[0].url}
+									alt={item.restaurant_name}
+								/>
+								<Stack p={4}>
+									<CardBody>
+										<Heading size="md">
+											{item.restaurant_name}
+										</Heading>
+										<Text py={2}>
+											Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.
+										</Text>
+									</CardBody>
+									<CardFooter>
+										<Button variant="solid" colorScheme="blue">
+											Buy Latte
+										</Button>
+									</CardFooter>
+								</Stack>
+							</Card>
+						))}
+				</Grid>
+			</div>
 		</div>
 	);
 };
--- fastor-restaurant-app/src/components/Otp.jsx
+++ fastor-restaurant-app/src/components/Otp.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
 import styles from './Otp.module.css';
+import {useNavigate} from 'react-router-dom';
 
 function Otp() {
+  const navigate =useNavigate();
   const [otp, setOtp] = useState(['', '', '', '', '', '']);
+  const [error, setError] = useState('');
   const inputRefs = useRef([]);
 
   useEffect(() => {
     inputRefs.current = inputRefs.current.slice(0, 6);
   }, []);
+
+  const correctOtp = '123456'; // Set the correct OTP value
 
   const handleChange = (e, index) => {
     const { value } = e.target;
@@ -32,43 +37,44 @@ function Otp() {
     }
   };
 
+  const handleSubmit = () => {
+    const enteredOtp = otp.join('');
+
+    if (enteredOtp === correctOtp) {
+      setError(''); // Reset error message
+      // Add your logic here when the OTP is correct (e.g., navigate to the next step)
+      navigate('/restaurants')
+    } else {
+      setError('Incorrect OTP. Please try again.');
+    }
+  };
+
   return (
     <div className={styles.containerOtp}>
-			
-			<div className={styles.boxOtp}>
-				<h2>OTP Verification</h2>
-				<p style={{fontWeight:'normal'}}> Enter the verification code we just sent on your Mobile Number.</p>
+      <div className={styles.boxOtp}>
+        <h2>OTP Verification</h2>
+        <p style={{ fontWeight: 'normal' }}>
+          Enter the verification code we just sent to your Mobile Number.
+        </p>
         <div className={styles.inputBox}>
-      {otp.map((digit, index) => (
-        <input
-          key={index}
-          ref={(ref) => (inputRefs.current[index] = ref)}
-          type="text"
-          value={digit}
-          onChange={(e) => handleChange(e, index)}
-          onKeyUp={(e) => handleBackspace(e, index)}
-          maxLength="1"
-        />
-      ))}
+          {otp.map((digit, index) => (
+            <input
+              key={index}
+              ref={(ref) => (inputRefs.current[index] = ref)}
+              type="text"
+              value={digit}
+              onChange={(e) => handleChange(e, index)}
+              onKeyUp={(e) => handleBackspace(e, index)}
+              maxLength="1"
+            />
+          ))}
+        </div>
+        <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+          Verify OTP
+        </button>
+        {error && <div className={styles.error}>{error}</div>}
+      </div>
     </div>
-				<button type="button" class="btn btn-primary">
-					Send Code
-				</button>
-			</div>
-		</div>
-    // <div>
-    //   {otp.map((digit, index) => (
-    //     <input
-    //       key={index}
-    //       ref={(ref) => (inputRefs.current[index] = ref)}
-    //       type="text"
-    //       value={digit}
-    //       onChange={(e) => handleChange(e, index)}
-    //       onKeyUp={(e) => handleBackspace(e, index)}
-    //       maxLength="1"
-    //     />
-    //   ))}
-    // </div>
   );
 }
 
