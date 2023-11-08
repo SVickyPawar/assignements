8a95e68806d881ce76d6b72bb6e290cd5bc485ef
Vivek Pawar <vickypawar2609@gmail.com>
2023-11-08 22:01:30 +0530

    fetchdata function init

--- masai-cp-problems-2385-1853-kjeaLJ-c7d3c07c6b6829acb8a18b33877ac49ccbe6c0a0/src/App.jsx
+++ masai-cp-problems-2385-1853-kjeaLJ-c7d3c07c6b6829acb8a18b33877ac49ccbe6c0a0/src/App.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+
 function App() {
+  const [isTakeTest, setIsTakeTest] = useState(false);
+
+  const fetchData = fetch('https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-quiz').then((res)=>res.json()).then(data=>console.log(data))
+  useEffect(()=>{
+    fetchData();
+  },[])
+
   return <div className="App">{/* import Booklet component here */}</div>;
 }
 
