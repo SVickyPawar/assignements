5faa4a7a6cc556573873abed448884d3255b01eb
Vivek Pawar <vickypawar2609@gmail.com>
2023-12-03 13:02:39 +0530

    new app init for simfoni

---
+++ simfoni-app/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h3 className='text-lg font-bold underline '>Welcome to Simfoni with Tailwind CSS</h3>
+    </div>
+  );
+}
+
+export default App;
